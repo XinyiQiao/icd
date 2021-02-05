6fce98fd0351e7fd061fbc957d914805218c6cc5
Xinyi Qiao <xinyiq@umich.edu>
2021-02-04 23:43:41 -0500

    preprocess

--- icd/cli.py
+++ icd/cli.py
@@ -10,17 +10,34 @@ console script for icd.
 
 import click
 import sys
+import os
+import pathlib
+import pandas as pd
+from .icd import process
 
+@click.group()
+def main():
+    pass
 
-@click.command()
-def main(args=None):
-    '''
-    icd command line interface
-    '''
+@main.command()
+@click.argument('data_dir')
+@click.option('--output', '-o', help = "Output directory.")
+def preprocess(data_dir, output):
+    click.echo("preprocessing!")
+    # check existing directories and make all necessary directories
+    data_dir = pathlib.Path(data_dir)
+    if not os.path.isdir(data_dir):
+        click.echo(f"Error: {data_dir} is not a directory")
+        sys.exit(1)
+    if output is None:
+        output_dir = input_dir/"output" # use default
+    else:
+        output_dir = pathlib.Path(output)
+    os.makedirs(output_dir)
 
-    click.echo("update icd.cli.main")
-    return 0
-
+    admission = process(input_dir)
+    os.chdir(output_dir)
+    df.to_csv("output.csv", index=False)
 
 def entry_point():
     '''
--- icd/icd.py
+++ icd/icd.py
@@ -1,3 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import pandas as pd
+
+def process(input_dir):
+    # read in data
+    admission = pd.read_csv(input_dir/"ADMISSIONS.csv")
+    procedures_icd = pd.read_csv(input_dir/"PROCEDURES_ICD.csv")
+    diagnoses_icd = pd.read_csv(input_dir/"DIAGNOSES_ICD.csv")
+    
+    procedures_icd = procedures_icd[['SUBJECT_ID','HADM_ID','ICD9_CODE']]
+    admission.join(procedures_icd, on="HADM_ID")
+    diagnoses_icd = diagnoses_icd[['SUBJECT_ID','HADM_ID','ICD9_CODE']]
+    admission.join(diagnoses_icd, on="HADM_ID")
+    return admission
